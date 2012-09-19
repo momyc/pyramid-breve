18ef6bc7faee9e3d357fad5c5391a600b1e878c6
Alexander Kulakov <a.kulakov@mail.ru>
2012-09-19 06:44:16 -0400

    Bug fix for momyc/pyramid_breve#1

--- pyramid_breve.py
+++ pyramid_breve.py
@@ -7,41 +7,36 @@ from breve.tags import html
 
 
 def includeme(config):
-    template = Template(html.tags, xmlns=html.xmlns, doctype=html.doctype)
-    # is there a better way to get application package?
     loader = BreveAssetLoader(config.registry.__name__)
-    config.add_renderer('.b', BreveRendererFactory(template, loader))
+    config.add_renderer(u'.b', BreveRendererFactory(loader))
 
 
 class BreveRendererFactory(object):
     implements(IRendererFactory)
 
-    def __init__(self, template, loader):
-        self.template = template
+    def __init__(self, loader):
         self.loader = loader
 
     def __call__(self, info):
-        name = info.name
-
         def render(value, system):
             value.update(system)
-            return self.template.render(name, value, loader=self.loader)
-        
+            template = Template(html.tags, xmlns=html.xmlns, doctype=html.doctype)
+            return template.render(info.name, value, loader=self.loader)
         return render
 
 
 class BreveAssetLoader(object):
 
     def __init__(self, package):
-        self.resolver = AssetResolver(package)
+        self.resolve = AssetResolver(package).resolve
 
     def stat(self, name, root):
-        if name.endswith('.b.b'):
+        if name.endswith(u'.b.b'):
             name = name[:-2]
 
-        uid = self.resolver.resolve(name).abspath()
+        uid = self.resolve(name).abspath()
         timestamp = stat(uid).st_mtime
         return uid, timestamp
 
     def load(self, uid):
-        return file(uid, 'rb').read()
+        return file(uid, 'U').read()
