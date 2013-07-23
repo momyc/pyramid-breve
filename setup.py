3473c0e799d7f0701c4e8f6bc9d119a2a02e2955
Alexander Kulakov <a.kulakov@mail.ru>
2013-07-23 13:36:56 -0400

    Version update

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup, find_packages
 
-version = '0.5'
+version = '0.5.2'
 
 setup(name='pyramid_breve',
       version=version,
       description="Breve template engine renderer for Pyramid",
-      long_description="""Just call config.include('pyramid_breve') and use Breve templates as Pyramid view renderers""",
+      long_description="""Just call config.include('pyramid_breve') and use
+      Breve templates as Pyramid view renderers""",
       keywords='pyramid breve',
       author='Alexander Kulakov',
-      author_email='a.kulakov@mail.ru',
+      author_email='homo.programmerus@gmail.com',
       url='http://github.com/momyc/pyramid-breve',
       license='MIT',
       py_modules=['pyramid_breve'],
-      install_requires=['breve', 'pyramid'],
+      install_requires=['breve', 'pyramid>=1.3'],
       )
